511a7bbc26a5aa9e72d30a66f2c66dd60e258401
nomitari <nomitari@gmail.com>
2020-06-10 18:04:17 -0600

    server and update

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 app.use(express.static("spotify-gradient")); // myApp will be the same folder name.
 app.get('/', function (req, res) {
- res.redirect('/'); 
+ res.redirect('/callback.html'); 
 });
 app.listen(process.env.PORT || 3000, 
 	() => console.log("Server is running..."));
