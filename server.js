d5c8e41b446ac17e88088639eb388e6983ca0862
nomitari <nomitari@gmail.com>
2020-06-10 18:33:49 -0600

    server update

--- server.js
+++ server.js
@@ -74,49 +74,49 @@
 
 
 
-var express = require('express');
-var app = express();
+/// var express = require('express');
+/// var app = express();
 
-app.set('port', (process.env.PORT || 5000));
-app.use(express.static(__dirname + '/spotify-gradient'));
+/// app.set('port', (process.env.PORT || 5000));
+/// app.use(express.static(__dirname + '/spotify-gradient'));
 
-app.get('/', function(request, response) {
-response.send('Hello World!');
-});
+/// app.get('/', function(request, response) {
+/// response.send('Hello World!');
+/// });
 
-var bodyParser = require('body-parser');
+/// var bodyParser = require('body-parser');
 
-var WEBHOOK_SECRET = "62DZWMCCFFHTTQ44CG3WUQ94CTT7GAAN";
+/// var WEBHOOK_SECRET = "62DZWMCCFFHTTQ44CG3WUQ94CTT7GAAN";
 
-app.post('/telerivet/webhook', bodyParser.urlencoded({ extended:    true     }),function(req, res) {
-   var secret = req.body.secret;
-   if (secret !== WEBHOOK_SECRET) {
-     res.status(403).end();
-     return;
-   }
+/// app.post('/telerivet/webhook', bodyParser.urlencoded({ extended:    true     }),function(req, res) {
+///    var secret = req.body.secret;
+///    if (secret !== WEBHOOK_SECRET) {
+///      res.status(403).end();
+///      return;
+///    }
 
-   if (req.body.event == 'incoming_message') {
+///    if (req.body.event == 'incoming_message') {
 
-     var content = req.body.content;
-     var from_number = req.body.from_number;
-     var phone_id = req.body.phone_id;
+///      var content = req.body.content;
+///      var from_number = req.body.from_number;
+///      var phone_id = req.body.phone_id;
 
-   // do something with the message, e.g. send an autoreply
-      res.json({
-       messages: [
-       { content: "Thanks for your message!,Stay Tuned for Awesome " }
-     ]
-   });
+///    // do something with the message, e.g. send an autoreply
+///       res.json({
+///        messages: [
+///        { content: "Thanks for your message!,Stay Tuned for Awesome " }
+///      ]
+///    });
 
-    }  
+///     }  
 
-    res.status(200).end();
-    }
-     );
+///     res.status(200).end();
+///     }
+///      );
 
-        app.listen(app.get('port'), function() {
-       console.log('Node app is running on port', app.get('port'));
-        });
+///         app.listen(app.get('port'), function() {
+///        console.log('Node app is running on port', app.get('port'));
+///         });
 
 
 
