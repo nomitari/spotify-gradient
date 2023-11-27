54980d5ffcbab4f9f0fc5d11595e7c30e80b53c3
Nomi Tari <42783402+nomitari@users.noreply.github.com>
2023-11-27 14:37:25 +0100

    Update redirect uri for vercel

--- services/auth.js
+++ services/auth.js
@@ -12,7 +12,7 @@
 			REDIRECT_URI = 'http://localhost:8000/callback.html';
 		} else {
 			CLIENT_ID = '7887b3adc6c34fae83c95722a5dae69e';
-			REDIRECT_URI = 'https://spotify-gradient.herokuapp.com/callback.html';
+			REDIRECT_URI = 'https://spotify-gradient.vercel.app/callback.html';
 		}
 
 		function getLoginURL(scopes) {
